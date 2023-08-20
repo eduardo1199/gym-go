1041be1817a5dc4e702e506748034fb17b7c1a6f
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-08-20 08:09:02 -0300

    create project gym-go

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  plugins: [],
+}

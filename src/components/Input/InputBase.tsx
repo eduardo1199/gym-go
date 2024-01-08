4f3d3a57f8d1cfbbdcaa6fc4d4244904e4ad6b9e
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-01-08 12:38:37 -0300

    feat: adapter input component error

---
+++ src/components/Input/InputBase.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+  error?: boolean
+}
+
+const InputBase = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, label, children, error, ...props }, ref) => {
+    return (
+      <div>
+        {label && (
+          <label
+            htmlFor={props.id}
+            className="text-sm text-primary-blue font-bold"
+          >
+            {label}
+          </label>
+        )}
+        <input
+          type={type}
+          id={props.id}
+          className={cn(
+            `flex w-full rounded-md border-input bg-background px-3 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-opacity-70 placeholder:text-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-primary-gray font-bold border  placeholder:text-tertiary-gray ${
+              error ? 'border-red-500' : 'border-tertiary-gray'
+            }`,
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {children}
+      </div>
+    )
+  },
+)
+InputBase.displayName = 'Input'
+
+export { InputBase }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,38 +1,7 @@
-import * as React from 'react'
+import { InputBase } from './InputBase'
+import { InputMessageError } from './InputMessageError'
 
-import { cn } from '@/lib/utils'
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: string
+export const Input = {
+  Root: InputBase,
+  Error: InputMessageError,
 }
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, label, ...props }, ref) => {
-    return (
-      <>
-        {label && (
-          <label
-            htmlFor={props.id}
-            className="text-sm text-primary-blue font-bold"
-          >
-            {label}
-          </label>
-        )}
-        <input
-          type={type}
-          id={props.id}
-          className={cn(
-            'flex w-full rounded-md border-input bg-background px-3 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-opacity-70 placeholder:text-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-primary-gray font-bold border border-tertiary-gray placeholder:text-tertiary-gray',
-            className,
-          )}
-          ref={ref}
-          {...props}
-        />
-      </>
-    )
-  },
-)
-Input.displayName = 'Input'
-
-export { Input }

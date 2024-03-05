d112ad2f3a6c572068e2b11b4b41a29423d57aa6
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-03-05 09:05:50 -0300

    feat: env validate zod

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import z from 'zod'
+
+const envSchema = z.object({
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  API_DATABASEURL: z.string().url(),
+  NEXT_PUBLIC_GOOGLE_MAPS_KEY: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('Invalid environment variable')
+
+  throw new Error('Invalid environment variable')
+}
+
+export const env = envSchema.parse(process.env)

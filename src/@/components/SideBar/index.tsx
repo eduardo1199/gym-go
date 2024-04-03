1706b3fb43d891c41bf6ffba03461f2d99131e54
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-04-03 09:47:29 -0300

    feat: aside bar layout component

---
+++ src/@/components/SideBar/index.tsx
@@ -0,0 +1,9 @@
+export function SideBar() {
+  return (
+    <aside className="bg-primary-blue rounded-r-3xl w-[80px] h-[95%] flex absolute z-50 top-5 shadow-lg shadow-primary-blue flex-col items-center">
+      <strong className="text-base font-bold text-primary-white animate-visible mt-3">
+        Gym<span className="text-primary-purple">Go</span>
+      </strong>
+    </aside>
+  )
+}

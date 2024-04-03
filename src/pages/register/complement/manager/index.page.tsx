c815fdabc7ae72c14651d8bfea4446d9107b4506
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-04-03 09:14:56 -0300

    feat: added lt and long on form state value

--- src/pages/register/complement/manager/index.page.tsx
+++ src/pages/register/complement/manager/index.page.tsx
@@ -4,9 +4,28 @@ import { Input } from '@/components/Input'
 import { env } from 'env'
 import { useEffect, useMemo, useState } from 'react'
 
+import * as Accordion from '@radix-ui/react-accordion'
+import { Checkbox } from '@/components/ui/checkbox'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const complementRegisterManagerGymSchema = z.object({
+  latitude: z.number(),
+  longitude: z.number(),
+})
+
+type ComplementRegisterManagerGymData = z.infer<
+  typeof complementRegisterManagerGymSchema
+>
+
 export default function ComplementRegisterManager() {
-  const [longitude, setLongitude] = useState<number>()
-  const [latitude, setLatitude] = useState<number>()
+  const { setValue, handleSubmit } = useForm<ComplementRegisterManagerGymData>({
+    resolver: zodResolver(complementRegisterManagerGymSchema),
+  })
+
+  const [currentLongitude, setCurrentLongitude] = useState<number>()
+  const [currentLatitude, setCurrentLatitude] = useState<number>()
 
   const [markerLatLng, setMarkerLatLng] = useState<google.maps.LatLng>()
 
@@ -14,19 +33,27 @@ export default function ComplementRegisterManager() {
 
   function handleMarkerInMap(event: google.maps.MapMouseEvent) {
     setMarkerLatLng(event.latLng)
+
+    const { lat, lng } = event.latLng
+
+    const latitude = lat()
+    const longitude = lng()
+
+    setValue('latitude', latitude)
+    setValue('longitude', longitude)
   }
 
   const mapCenter = useMemo(
-    () => ({ lat: latitude, lng: longitude }),
-    [latitude, longitude],
+    () => ({ lat: currentLatitude, lng: currentLongitude }),
+    [currentLatitude, currentLongitude],
   )
 
   useEffect(() => {
     const geoLocation = navigator.geolocation
 
     geoLocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude)
-      setLongitude(position.coords.longitude)
+      setCurrentLatitude(position.coords.latitude)
+      setCurrentLongitude(position.coords.longitude)
     })
   }, [])
 
@@ -44,6 +71,12 @@ export default function ComplementRegisterManager() {
     libraries: libraries as any,
   })
 
+  function handleSubmitRegisterComplementManager(
+    data: ComplementRegisterManagerGymData,
+  ) {
+    console.log(data)
+  }
+
   if (!isLoaded) {
     return <p>Loading</p>
   }
@@ -60,33 +93,140 @@ export default function ComplementRegisterManager() {
             Preencha as informações da sua academia!
           </span>
 
-          <form action="" className="flex flex-col gap-4 w-full">
+          <form
+            action=""
+            className="flex flex-col gap-4 w-full"
+            onSubmit={handleSubmit(handleSubmitRegisterComplementManager)}
+          >
             <Input.Root
-              placeholder="Nome"
+              placeholder="Nome da academia"
               className="bg-transparent text-white"
             >
               <Input.Error />
             </Input.Root>
             <Input.Root
-              placeholder="Nome"
-              className="bg-transparent text-white"
-            >
-              <Input.Error />
-            </Input.Root>
-            <Input.Root
-              placeholder="Nome"
-              className="bg-transparent text-white"
-            >
-              <Input.Error />
-            </Input.Root>
-            <Input.Root
-              placeholder="Nome"
+              placeholder="CNPJ"
               className="bg-transparent text-white"
             >
               <Input.Error />
             </Input.Root>
 
-            <Button.Primary className="mt-4">Cadastrar</Button.Primary>
+            <span className="text-muted-foreground text-white text-sm font-semibold">
+              Selecione os horários disponíveis da academia
+            </span>
+
+            <div className="space-y-3">
+              <Accordion.Root
+                type="single"
+                collapsible
+                className="border border-primary-purple transition-all rounded bg-transparent over:bg-secondary-blue hover:shadow-md hover:shadow-tertiary-purple"
+              >
+                <Accordion.Item value="monday">
+                  <Accordion.Trigger className="text-primary-purple font-semibold text-sm p-2 hover:brightness-105 w-full flex">
+                    Segunda-feira
+                  </Accordion.Trigger>
+
+                  <Accordion.Content className="p-2">
+                    <div className="grid gap-3 grid-cols-3">
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+                    </div>
+                  </Accordion.Content>
+                </Accordion.Item>
+              </Accordion.Root>
+              <Accordion.Root
+                type="single"
+                collapsible
+                className="border border-primary-purple transition-all rounded bg-transparent over:bg-secondary-blue hover:shadow-md hover:shadow-tertiary-purple"
+              >
+                <Accordion.Item value="monday">
+                  <Accordion.Trigger className="text-primary-purple font-semibold text-sm p-2 hover:brightness-105 w-full flex">
+                    Terça-feira
+                  </Accordion.Trigger>
+
+                  <Accordion.Content className="p-2">
+                    <div className="grid gap-3 grid-cols-3">
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <Checkbox id="monday" />
+                        <label
+                          htmlFor="monday"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white"
+                        >
+                          05:00h
+                        </label>
+                      </div>
+                    </div>
+                  </Accordion.Content>
+                </Accordion.Item>
+              </Accordion.Root>
+            </div>
+
+            <Button.Primary className="mt-4" type="submit">
+              Cadastrar
+            </Button.Primary>
           </form>
         </div>
         <div className="bg-white w-[70%] h-full">

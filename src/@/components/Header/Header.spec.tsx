43051ab4e9d624d583aaae7f38a0b27d6f0d8f69
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-04-24 19:15:37 -0300

    feat: testing header component

--- src/@/components/Header/Header.spec.tsx
+++ src/@/components/Header/Header.spec.tsx
@@ -1,41 +1,17 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { render } from '@testing-library/react'
 import { Header } from '.'
 
-vi.mock('next/navigation', () => {
-  return {
-    useRouter() {
-      return {
-        push() {
-          return {}
-        },
-      }
-    },
-  }
-})
-
 describe('testing component Header', () => {
-  it('should render button register', async () => {
+  it('should render button register and about and login', async () => {
     const { getByTitle } = render(<Header />)
 
-    const buttonRegister = getByTitle('register')
+    const registerLink = getByTitle('register')
+    const aboutLink = getByTitle('about')
+    const loginLink = getByTitle('login')
 
-    expect(buttonRegister).toBeInTheDocument()
-  })
-
-  it('should render button about', async () => {
-    const { getByTitle } = render(<Header />)
-
-    const buttonRegister = getByTitle('about')
-
-    expect(buttonRegister).toBeInTheDocument()
-  })
-
-  it('should render button login', async () => {
-    const { getByTitle } = render(<Header />)
-
-    const buttonRegister = getByTitle('login')
-
-    expect(buttonRegister).toBeInTheDocument()
+    expect(registerLink).toBeInTheDocument()
+    expect(aboutLink).toBeInTheDocument()
+    expect(loginLink).toBeInTheDocument()
   })
 })
---
+++ src/@/components/NavigateLink/navigate-link.spec.tsx
@@ -0,0 +1,16 @@
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
+import { fireEvent, render } from '@testing-library/react'
+import { NavigateLink } from './index'
+
+describe('testing component navigate link', () => {
+  it('should render button register page', async () => {
+    /* const { getByText } = render(
+      <NavigateLink href={'/register'} text="Cadastro" />,
+    )
+
+    const registerLink = getByText('Cadastro')
+    expect(registerLink).toBeInTheDocument()
+
+    fireEvent.click(registerLink) */
+  })
+})
